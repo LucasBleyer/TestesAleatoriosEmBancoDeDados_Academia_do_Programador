726ba3d7a890b181695f2cbc51cd0857fefa0488
Lucas Bleyer <lucasbleyer19@gmail.com>
2022-05-30 09:21:08 -0300

    repositorio materia atualizado

--- TestesAleatorios.Infra.BancoDeDados/ModuloMateria/RepositorioMateriaEmBancoDeDados.cs
+++ TestesAleatorios.Infra.BancoDeDados/ModuloMateria/RepositorioMateriaEmBancoDeDados.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +11,186 @@ namespace TestesAleatorios.Infra.BancoDeDados.ModuloMateria
 {
     public class RepositorioMateriaEmBancoDeDados : IRepositorioMateria
     {
+        private const string enderecoBanco = "Data Source=(LocalDB)\\MSSqlLocalDB;Initial Catalog=TestesDB;Integrated Security=True;Pooling=False";
 
+        #region Sql Queries
+        private const string sqlInserir =
+            @"INSERT INTO [TBDISCIPLINA] 
+                (
+                    [NOME]
+	            )
+	            VALUES
+                (
+                    @NOME
+                );SELECT SCOPE_IDENTITY();";
+
+        private const string sqlEditar =
+            @"UPDATE [TBDISCIPLINA]	
+		        SET
+			        [NOME] = @NOME
+		        WHERE
+			        [NUMERO] = @NUMERO";
+
+        private const string sqlExcluir =
+            @"DELETE FROM [TBDISCIPLINA]
+		        WHERE
+			        [NUMERO] = @NUMERO";
+
+        private const string sqlSelecionarTodos =
+            @"SELECT 
+		            [NUMERO],
+                    [NOME]
+	            FROM 
+		            [TBDISCIPLINA]";
+
+        private const string sqlSelecionarPorNumero =
+            @"SELECT 
+		            [NUMERO],
+                    [NOME]
+	            FROM 
+		            [TBDISCIPLINA]
+		        WHERE
+                    [NUMERO] = @NUMERO";
+        #endregion
+
+        public ValidationResult Inserir(Materia novaMateria)
+        {
+            var validador = new ValidadorMateria();
+
+            var resultadoValidacao = validador.Validate(novaMateria);
+
+            if (resultadoValidacao.IsValid == false)
+                return resultadoValidacao;
+
+            SqlConnection conexaoComBanco = new SqlConnection(enderecoBanco);
+
+            SqlCommand comandoInsercao = new SqlCommand(sqlInserir, conexaoComBanco);
+
+            ConfigurarParametrosDisciplina(novaMateria, comandoInsercao);
+
+            conexaoComBanco.Open();
+
+            var id = comandoInsercao.ExecuteScalar();
+            //utilizado para executar instruções SQL como count, sum, min, esse método retorna apenas um valor como resultado
+
+            novaMateria.Numero = Convert.ToInt32(id);
+
+            conexaoComBanco.Close();
+
+            return resultadoValidacao;
+        }
+
+        public ValidationResult Editar(Materia materia)
+        {
+            var validador = new ValidadorMateria();
+
+            var resultadoValidacao = validador.Validate(materia);
+
+            if (resultadoValidacao.IsValid == false)
+                return resultadoValidacao;
+
+            SqlConnection conexaoComBanco = new SqlConnection(enderecoBanco);
+
+            SqlCommand comandoEdicao = new SqlCommand(sqlEditar, conexaoComBanco);
+
+            ConfigurarParametrosDisciplina(materia, comandoEdicao);
+
+            conexaoComBanco.Open();
+
+            comandoEdicao.ExecuteNonQuery();
+            //é utilizado para executar instruções SQL que não retornam dados
+            conexaoComBanco.Close();
+
+            return resultadoValidacao;
+        }
+
+        public ValidationResult Excluir(Materia materia)
+        {
+            SqlConnection conexaoComBanco = new SqlConnection(enderecoBanco);
+
+            SqlCommand comandoExclusao = new SqlCommand(sqlExcluir, conexaoComBanco);
+
+            comandoExclusao.Parameters.AddWithValue("NUMERO", materia.Numero);
+
+            conexaoComBanco.Open();
+
+            int numeroRegistrosExcluidos = comandoExclusao.ExecuteNonQuery();
+            //é utilizado para executar instruções SQL que não retornam dados
+
+            var resultadoValidacao = new ValidationResult();
+
+            if (numeroRegistrosExcluidos == 0)
+                resultadoValidacao.Errors.Add(new ValidationFailure("", "Não foi possível remover o registro"));
+
+            conexaoComBanco.Close();
+
+            return resultadoValidacao;
+        }
+
+        public List<Materia> SelecionarTodos()
+        {
+            SqlConnection conexaoComBanco = new SqlConnection(enderecoBanco);
+
+            SqlCommand comandoSelecao = new SqlCommand(sqlSelecionarTodos, conexaoComBanco);
+
+            conexaoComBanco.Open();
+
+            SqlDataReader leitorMateria = comandoSelecao.ExecuteReader();
+            //método utilizado para execultar uma instrução SQL que retorna um DataReader
+
+            List<Materia> materias = new List<Materia>();
+
+            while (leitorMateria.Read())
+            {
+                Materia materia = ConverterParaDisciplina(leitorMateria);
+
+                materias.Add(materia);
+            }
+
+            conexaoComBanco.Close();
+
+            return materias;
+        }
+
+        public Materia SelecionarPorNumero(int numero)
+        {
+            SqlConnection conexaoComBanco = new SqlConnection(enderecoBanco);
+
+            SqlCommand comandoSelecao = new SqlCommand(sqlSelecionarPorNumero, conexaoComBanco);
+
+            comandoSelecao.Parameters.AddWithValue("NUMERO", numero);
+
+            conexaoComBanco.Open();
+            SqlDataReader leitorMateria = comandoSelecao.ExecuteReader();
+            //método utilizao para execultar uma instrução SQL que retorna um DataReader
+
+            Materia materia = null;
+            if (leitorMateria.Read())
+                materia = ConverterParaDisciplina(leitorMateria);
+
+            conexaoComBanco.Close();
+
+            return materia;
+        }
+
+        private static Materia ConverterParaDisciplina(SqlDataReader leitorDisciplina)
+        {
+            int numero = Convert.ToInt32(leitorDisciplina["NUMERO"]);
+            string nome = Convert.ToString(leitorDisciplina["NOME"]);
+
+            var materia = new Materia
+            {
+                Numero = numero,
+                Nome = nome,
+            };
+
+            return materia;
+        }
+
+        private static void ConfigurarParametrosDisciplina(Materia novaDisciplina, SqlCommand comando)
+        {
+            comando.Parameters.AddWithValue("NUMERO", novaDisciplina.Numero);
+            comando.Parameters.AddWithValue("NOME", novaDisciplina.Nome);
+        }
     }
 }
